612bca6ce01e93e1f0509c3f1f5d4ab56b0e657a
bertaye <bertayeren@gmail.com>
2024-05-21 00:04:48 +0300

    Create a lightweit GPU Instancing script

---
+++ Assets/Scripts/Instancing/TerrainGPUInstancing.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class TerrainGPUInstancing : MonoBehaviour
+{
+    [SerializeField] Material material;
+    [SerializeField] Mesh instanceMesh;
+    public static TerrainGPUInstancing Instance { get; private set; }
+    List<Matrix4x4> instanceMatrices = new List<Matrix4x4>();
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+    public int AddTerrainChunkInstance(Matrix4x4 trsMatrix)
+    {
+        instanceMatrices.Add(trsMatrix);
+        return instanceMatrices.Count - 1; //This will serve as an ID for the instance
+    }
+
+    public void RemoveTerrainChunkInstance(int id)
+    {
+        instanceMatrices.RemoveAt(id);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Graphics.DrawMeshInstanced(instanceMesh, 0, material, instanceMatrices);
+    }
+}
